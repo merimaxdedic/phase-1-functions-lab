9092c1cff2cbf6a38c37c8ab28284154cf509a2d
Merima Dedic <merimaxdedic@gmail.com>
2023-11-05 22:32:00 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(currentStreet) {
+    const headquartersStreet = 42;
+  
+    if (currentStreet < 42) {
+      return headquartersStreet - currentStreet;
+    } else {
+      return currentStreet - headquartersStreet;
+    }
+  }
+  function distanceFromHqInFeet(currentStreet) {
+    const feetPerBlock = 264;
+    const blocks = distanceFromHqInBlocks(currentStreet);
+    return blocks * feetPerBlock;
+  }
+
+  function distanceTravelledInFeet(startingPoint, destination) {
+    const feetPerBlock = 264;
+  
+    let distanceInBlocks;
+    if (destination < startingPoint) {
+      distanceInBlocks = startingPoint - destination;
+    } else {
+      distanceInBlocks = destination - startingPoint;
+    }
+  
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+  
+    return distanceInFeet;
+  }
+ 
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
